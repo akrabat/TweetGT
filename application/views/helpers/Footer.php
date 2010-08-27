53d09c0d04ea5d4a4308844d087fe51b8ffd6a2b
Rob Allen <rob@akrabat.com>
2010-08-27 20:13:56 +0100

    minor update of footer

--- application/views/helpers/Footer.php
+++ application/views/helpers/Footer.php
@@ -9,9 +9,10 @@ class Zend_View_Helper_Footer extends Zend_View_Helper_Abstract
             $year = "2010, $year";
         }
         return <<<EOT
-    <p>Copyright &copy; $year by <a href="http://akrabat.com">Rob Allen</a>. All rights reserved.<br/>
-    This application is built with <a href="http://framework.zend.com">Zend Framwork</a>. 
-    Source code is available on <a href="http://www.github.com/akrabat/TweetGT">github</a>.
+    <p>Copyright &copy; $year by <a href="http://akrabat.com">Rob Allen</a>.<br/>
+    This application is built with <a href="http://framework.zend.com">Zend Framework</a>.
+    Source code is available on <a href="http://www.github.com/akrabat/TweetGT">github</a> 
+    and licensed under the <a href="http://akrabat.com/license/new-bsd/">New BSD</a>.
 
 
 EOT;
